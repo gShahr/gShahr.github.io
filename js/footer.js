a95ceadcbb96124e3f5b2a046124532127e5ec94
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2025-02-27 00:46:26 -0500

    Update index.html and footer.js; add work section and modify footer copyright

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@
 function insertFooter() {
     const footerHtml = `
     <footer id="contact">
-        <p>&copy; <span id="current-year"></span> gShahr. All rights reserved.</p>
+        <p>&copy; <span id="current-year"></span> Theriatric. All rights reserved.</p>
         <div id="socials">
             <a href="https://github.com/gShahr" target="_blank">GitHub</a> |
             <a href="https://www.linkedin.com/in/gabriel-s-902134197/" target="_blank">LinkedIn</a>
